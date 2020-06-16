d4e64ea8b8078505eef5649953b5d6c1a110dc34
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-06-16 15:48:08 +0200

    structure + styles CardDesign

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 // == Import
+import CardDesign from 'src/components/CardDesign';
 import reactLogo from './react-logo.svg';
 import './styles.css';
 
@@ -10,6 +11,7 @@ const App = () => (
   <div className="app">
     <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
+    <CardDesign />
   </div>
 );
 
