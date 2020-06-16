c094c744f5a682003f04bff31f9428e7e226f041
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-06-16 23:07:32 +0200

    productpage/on going

--- front/src/components/ProductPage/index.js
+++ front/src/components/ProductPage/index.js
@@ -11,19 +11,15 @@ const ProductPage = () => (
     <header className="productpage-header">
       <p>HEADER</p>
     </header>
-    <div className="productpage-photo">
-      <p>Photo</p>
-    </div>
+    <img className="productpage-photo" src="https://i.ebayimg.com/images/g/jZYAAOSw9ytdU9mt/s-l300.jpg" alt="parrain" />
+    <p className="productpage-user">Le Sicilien - 23 évaluations</p>
     <section className="productpage-infos">
-      <h1 className="productpage-infos--title">NOM DU PRODUIT</h1>
-      <p className="productpage-infos--item">Prix :</p>
-      <p className="productpage-infos--item">Loué par :</p>
-      <p className="productpage-infos--item">Marque :</p>
-      <p className="productpage-infos--item">Taille :</p>
-      <p className="productpage-infos--item">État du produit :</p>
+      <h1 className="productpage-infos--title">Déguisement de parrain de la street</h1>
+      <p className="productpage-infos--item">M / 38 / 10 - Impeccable - Giorgio Armani</p>
+      <p className="productpage-infos--price">1.000.000 $</p>
     </section>
     <section className="productpage-button">
-      <Button size="small" value="Louer" />
+      <Button size="large" value="Louer" />
     </section>
     <section className="others-products">
       <p>AUTRES PRODUITS DE LA MÊME FAMILLE</p>
