b2f43519c7f0ee13dc4b8cd126e45813a70928b4
roman974 <roman.c6lle@gmail.com>
2020-06-16 17:17:08 +0200

    user page without card/Done

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -3,13 +3,18 @@ import React from 'react';
 
 // == Import components
 import Button from 'src/components/Button';
+import UserPage from 'src/components/UserPage';
+
 
 // == Import
 import './styles.scss';
 
 // == Composant
 const App = () => (
-  <Button value="Envoyer" size="medium" />
+  // <Button value="Envoyer" size="medium" />
+  // <Avatar size="large" />
+  <UserPage />
+
 );
 
 // == Export
