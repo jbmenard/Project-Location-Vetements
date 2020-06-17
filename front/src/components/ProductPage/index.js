c7434263553177673bc262e87aafe4a4792a82da
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-06-17 10:19:24 +0200

    component-product-page/done

--- front/src/components/ProductPage/index.js
+++ front/src/components/ProductPage/index.js
@@ -12,17 +12,24 @@ const ProductPage = () => (
       <p>HEADER</p>
     </header>
     <img className="productpage-photo" src="https://i.ebayimg.com/images/g/jZYAAOSw9ytdU9mt/s-l300.jpg" alt="parrain" />
-    <p className="productpage-user">Le Sicilien - 23 évaluations</p>
+    <section className="productpage-user">
+      <img className="productpage-user--photo" src="https://image.flaticon.com/icons/png/512/64/64572.png" alt="avatar" />
+      <div className="productpage-user--infos">
+        <p className="productpage-user--name">Le Sicilien</p>
+        <p className="productpage-user--note">23 évaluations - 4,3 / 5</p>
+      </div>
+    </section>
     <section className="productpage-infos">
       <h1 className="productpage-infos--title">Déguisement de parrain de la street</h1>
       <p className="productpage-infos--item">M / 38 / 10 - Impeccable - Giorgio Armani</p>
       <p className="productpage-infos--price">1.000.000 $</p>
     </section>
     <section className="productpage-button">
-      <Button size="large" value="Louer" />
+      <Button size="medium" value="Louer" />
     </section>
-    <section className="others-products">
-      <p>AUTRES PRODUITS DE LA MÊME FAMILLE</p>
+    <section className="productpage-suggest">
+      <div className="productpage-suggest--item">PRODUIT 1</div>
+      <div className="productpage-suggest--item">PRODUIT 2</div>
     </section>
     <footer className="productpage-header">
       <p>FOOTER</p>
