48b1d50ef8cd82425bcd444e6aa9fedbcbc7c66a
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-06-25 07:37:36 +0200

    modifi composant home /on going

---
+++ front/src/components/Home/Opinion/index.js
@@ -0,0 +1,15 @@
+import React from 'react ';
+import './styles.scss';
+
+const Opinion = () => (
+
+    <div className="comment col-md-4 ">
+          <i className="fa fa-user" />
+          <h3 className="comment-title"> Martin Dupont</h3>
+          <i className="icon-star">★★★★★</i>
+          <p className="comment-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt neque, tenetur, delectus necessitatibus itaque voluptatem suscipit voluptatum rem, quaerat ipsa asperiores quo velit voluptas veritatis. Labore nesciunt dolores totam possimus.</p>
+        </div>
+);
+
+
+export default Opinion ; 
--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -1,38 +1,48 @@
 import React from 'react';
 import './styles.scss';
-
+//---------------------- composants -----------------------------//
+import Opinion from './Opinion';
 import Cards from './Cards';
+// ----------------carrousel version desktop ---------------------//
+
 import Watch from '../../assets/images/watch.jpg';
 import Ski from '../../assets/images/ski.jpg';
 import Wedding from '../../assets/images/wedding.jpg';
+// ----------------carrousel version mobile ---------------------//
+
+import WatchMobile from '../../assets/images/watch-mobile.jpg';
+import WeddingMobile from '../../assets/images/wedding-mobile.jpg';
+import SkiMobile from '../../assets/images/ski-mobile.jpg';
+// ------------------cards section service ---------------------//
+
 import Loue from '../../assets/images/je-loue.PNG';
 import Reçois from '../../assets/images/je-recois.PNG';
 import Rends from '../../assets/images/je-rends.png';
 
 const Home = () => (
-  // --------------- carousel ---------------
+
   <>
     <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img className="d-block img-fluid" src={Watch} alt="First slide" />
+          <img className="d-block  img-responsive img-fluid" src={WatchMobile} alt="First slide" />
           <div className="carousel-caption d-md-block">
-            <h5>TITLE</h5>
-            <p>PHRASE D'ACCROCHE</p>
+            <h5 className="carousel-title">Envie d'une montre de Luxe ?</h5>
+            {/* <p>PHRASE D'ACCROCHE</p> */}
           </div>
         </div>
         <div className="carousel-item">
-          <img className="d-block  img-fluid" src={Ski} alt="Second slide" />
+          <img className="d-block img-responsive img-fluid" src={SkiMobile} alt="Second slide" />
           <div className="carousel-caption  d-md-block">
-            <h5>TITLE</h5>
-            <p>PHRASE D'ACCROCHE</p>
+            <h5 className="carousel-title">Besoin d'un equipement ?</h5>
+            {/* <p>PHRASE D'ACCROCHE</p> */}
           </div>
         </div>
         <div className="carousel-item">
-          <img className="d-block  img-fluid" src={Wedding} alt="Third slide" />
+          <img className="d-block img-responsive img-fluid" src={WeddingMobile} alt="Third slide" />
           <div className="carousel-caption  d-md-block">
-            <h5>TITLE</h5>
-            <p>PHRASE D'ACCROCHE</p>
+            <h5 className="carousel-title">Invitée à un mariage ? </h5>
+            {/* <p className="carousel-text">Louer votre tenue </p> */}
           </div>
         </div>
       </div>
@@ -55,28 +65,28 @@ const Home = () => (
         <Cards title="JE RENDS" image={Rends} text="Faut rendre les affaires " number={3} />
       </div>
     </section>
-    <section className="container-temoignages">
+    <section className="container-comment">
       <h2 className="cards-title"> Temoignages </h2>
       <span className="invider" />
       <div className="row">
 
-        <div className="temoignages col-md-4 ">
+        <div className="comment col-md-4 ">
           <i className="fa fa-user" />
-          <h3 className="temoignages-title"> Martin Dupont</h3>
+          <h3 className="comment-title"> Martin Dupont</h3>
           <i className="icon-star">★★★★★</i>
-          <p className="temoignages-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt neque, tenetur, delectus necessitatibus itaque voluptatem suscipit voluptatum rem, quaerat ipsa asperiores quo velit voluptas veritatis. Labore nesciunt dolores totam possimus.</p>
+          <p className="comment-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt neque, tenetur, delectus necessitatibus itaque voluptatem suscipit voluptatum rem, quaerat ipsa asperiores quo velit voluptas veritatis. Labore nesciunt dolores totam possimus.</p>
         </div>
-        <div className="temoignages col-md-4 ">
+        <div className="comment col-md-4 ">
           <i className="fa fa-user" />
-          <h3 className="temoignages-title"> Juliette Smith</h3>
+          <h3 className="comment-title"> Juliette Smith</h3>
           <i className="icon-star">★★★★</i>
-          <p className="temoignages-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem vero dolores sequi unde, consequuntur iusto qui voluptatibus delectus blanditiis cumque voluptatem esse adipisci fuga accusantium et at quasi voluptas?</p>
+          <p className="comment-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem vero dolores sequi unde, consequuntur iusto qui voluptatibus delectus blanditiis cumque voluptatem esse adipisci fuga accusantium et at quasi voluptas?</p>
         </div>
-        <div className="temoignages col-md-4">
+        <div className="comment col-md-4">
           <i className="fa fa-user" />
-          <h3 className="temoignages-title">Fatima Abdallah</h3>
+          <h3 className="comment-title">Fatima Abdallah</h3>
           <i className="icon-star">★★★★★</i>
-          <p className="temoignages-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores nostrum soluta quasi adipisci temporibus facere nulla. Consectetur, quasi esse quos magni ut maxime aut libero rem voluptas! Nostrum, iste dolorum?</p>
+          <p className="comment-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores nostrum soluta quasi adipisci temporibus facere nulla. Consectetur, quasi esse quos magni ut maxime aut libero rem voluptas! Nostrum, iste dolorum?</p>
         </div>
       </div>
     </section>
