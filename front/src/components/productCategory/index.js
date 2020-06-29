3ccf02214e60174775ebafddbbf1b0d4ccb1d14b
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-06-29 18:40:16 +0200

    responsive/done

--- front/src/components/productCategory/index.js
+++ front/src/components/productCategory/index.js
@@ -8,46 +8,49 @@ console.log(listProducts);
 
   return (
     <>
-      <div className="product-event">
-        <label htmlFor="subCategory" className="label">
-          Catégory :
-          <select name="subCategory" id="subCategory">
-            <option value="sport">sport</option>
-            <option value="évènement">évènement</option>
-            <option value="Accessoires">Accessoires</option>
+      <section className="product-category-page">
+        <div className="product-event">
+          <label htmlFor="subCategory" className="label-item">
+            Catégorie :
+            <select name="subCategory" id="subCategory" className="label-select">
+              <option value="sport">sport</option>
+              <option value="évènement">évènement</option>
+              <option value="Accessoires">Accessoires</option>
 
-          </select>
-        </label>
-        <label htmlFor="subCategory" className="label">
-          Sous-Catégory :
-          <select name="subCategory" id="subCategory">
-            <option value="sportDeGlisse">sportDeGlisse</option>
-            <option value="soireeEtFete">soireeEtFete</option>
-            <option value="mariage">mariage</option>
-            <option value="maroquinerie">maroquinerie</option>
-          </select>
-        </label>
-      </div>
-      <>
-        {!loading && error && (
-          <p className="loading-error">Une erreur s'est produite.Veuillez réessayer plus tard.</p>
-        )}
-        {
-          !loading && !error && (
-          <article className="product-article">
-            {
-            listProducts.map((list) => (
-              <CardDesign object={list} />
+            </select>
+          </label>
+          <label htmlFor="subCategory" className="label-item">
+            Sous-catégorie :
+            <select name="subCategory" id="subCategory" className="label-select">
+              <option value="sportDeGlisse">sportDeGlisse</option>
+              <option value="soireeEtFete">soireeEtFete</option>
+              <option value="mariage">mariage</option>
+              <option value="maroquinerie">maroquinerie</option>
+            </select>
+          </label>
+        </div>
+        <>
+          {!loading && error && (
+            <p className="loading-error">Une erreur s'est produite.Veuillez réessayer plus tard.</p>
+          )}
+          {
+            !loading && !error && (
+            <article className="product-article">
+              {
+              listProducts.map((list) => (
+                <CardDesign object={list} />
 
-            ))
+              ))
+            }
+            </article>
+            )
           }
-          </article>
-          )
-        }
-        {loading && (
-          <p className="loading">Veuillez patientez. Les articles chargent.</p>
-        )}
-      </>
+          {loading && (
+            <p className="loading">Veuillez patientez. Les articles chargent.</p>
+          )}
+        </>
+      </section>
+
     </>
   );
 };
