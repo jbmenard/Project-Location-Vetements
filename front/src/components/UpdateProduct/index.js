c3cec11bc475f1a1faa1be28916e49503367c009
roman974 <roman.c6lle@gmail.com>
2020-07-01 10:10:22 +0200

    fixed bug on update/Done

--- front/src/components/UpdateProduct/index.js
+++ front/src/components/UpdateProduct/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import './style.scss';
 import { NavLink } from 'react-router-dom';
+import { object } from 'prop-types';
 
 const UpdateProduct = ({
   updateProduct,
@@ -15,7 +16,7 @@ const UpdateProduct = ({
   onSelectMark,
   onChangeDescription,
   onChangeImage,
-  id,
+  product,
 }) => {
   const handleSubmit = (event, productId) => {
     event.preventDefault();
@@ -58,31 +59,45 @@ const UpdateProduct = ({
     console.log(event.target.files[0]);
     onChangeImage(event.target.files[0], event.target.name);
   };
+  console.log(product);
+
   return (
     <div className="update-product">
       <h1 className="update-product--title">Modifier le produit</h1>
-      <form className="update-product--form" onSubmit={(event) => handleSubmit(event, id)}>
+      <form className="update-product--form" onSubmit={(event) => handleSubmit(event, product.id)}>
         <label htmlFor="name">
           Nom du produit :
-          <input className="update-product--input" type="text" name="name" id="name" onChange={handleChangeName} />
+          <input className="update-product--input" type="text" placeholder={product.name} name="name" id="name" onChange={handleChangeName} />
         </label>
         <label htmlFor="sub_category_id">
           Catégorie :
-          <select className="update-product--select" name="sub_category_id" id="sub_category_id" onChange={handleSelectSubCategory}>
-            <option value="0">Sports de Glisse</option>
-            <option value="7">Soirées et Fêtes</option>
+          <select className="update-product--select" defaultValue={product.sub_category} name="sub_category_id" id="sub_category_id" onChange={handleSelectSubCategory}>
+            <option value="">Choisir :</option>
+            <option value="1">Sports de Glisse</option>
+            <option value="2">Sports nautiques</option>
+            <option value="3">Equitation</option>
+            <option value="4">Sports de combat</option>
+            <option value="5">Sports mécaniques</option>
             <option value="6">Mariages</option>
+            <option value="7">Soirées et Fêtes</option>
+            <option value="8">Déguisements</option>
+            <option value="9">EVJ/EVG</option>
+            <option value="10">Enterrement</option>
             <option value="11">Maroquinerie</option>
+            <option value="12">Bijoux</option>
+            <option value="13">Chapeaux</option>
+            <option value="14">Sacs</option>
           </select>
         </label>
         <label htmlFor="mark">
           Marque :
-          <input onChange={handleSelectMark} className="update-product--input" type="text" name="mark" id="mark" />
+          <input onChange={handleSelectMark} className="update-product--input" placeholder={product.mark} type="text" name="mark" id="mark" />
         </label>
         <label htmlFor="gender_id">
-          Genre
-          <select className="update-product--select" name="gender_id" id="gender_id" onChange={handleSelectGender}>
-            <option value="0">Homme</option>
+          Sexe
+          <select className="update-product--select" defaultValue={product.gender} name="gender_id" id="gender_id" onChange={handleSelectGender}>
+            <option value="">Choisir : </option>
+            <option value="1">Homme</option>
             <option value="2">Femme</option>
             <option value="3">Enfant</option>
             <option value="4">Bébé</option>
@@ -90,36 +105,37 @@ const UpdateProduct = ({
         </label>
         <label htmlFor="size">
           Taille
-          <select className="update-product--select" name="size" id="size" onChange={handleSelectSize}>
-            <option value="xsmall">XS</option>
-            <option value="small">S</option>
-            <option value="medium">M</option>
-            <option value="large">L</option>
-            <option value="xlarge">XL</option>
-            <option value="2xlarge">XXL</option>
-            <option value="3xlarge">3XL</option>
+          <select className="update-product--select" defaultValue={product.size} name="size" id="size" onChange={handleSelectSize}>
+            <option value="">Choisir :</option>
+            <option value="XS">XS</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
           </select>
         </label>
         <label htmlFor="status">
           Etat du produit :
-          <select className="update-product--select" name="status" id="status" onChange={handleSelectState}>
-            <option value="neverUsed">Jamais porté</option>
-            <option value="likeNew">Comme neuf</option>
-            <option value="used">Usé</option>
-            <option value="toGive">A donner</option>
+          <select className="update-product--select" defaultValue={product.state} name="status" id="status" onChange={handleSelectState}>
+            <option value="">Choisir</option>
+            <option value="Jamais porté">Jamais porté</option>
+            <option value="Bon état">Bon état</option>
+            <option value="Usé">Usé</option>
+            <option value="Fin de vie">Fin de vie</option>
           </select>
         </label>
         <label htmlFor="description">
           Description :
-          <input onChange={handleChangeDescription} className="update-product--input" type="textarea" name="description" id="description" onChange={handleChangeName} />
+          <input onChange={handleChangeDescription} placeholder={product.description} className="update-product--input" type="textarea" name="description" id="description" onChange={handleChangeName} />
         </label>
         <label htmlFor="price">
           Prix :
-          <input className="update-product--input" type="number" name="price" id="price" onChange={handleChangePrice} />
+          <input className="update-product--input" placeholder={product.price} type="number" name="price" id="price" onChange={handleChangePrice} />
         </label>
         <label htmlFor="price">
           Image :
-          <input className="update-product--input" multiple="multiple" type="file" name="image" id="image" onChange={handleChangeImage} />
+          <input className="update-product--input" multiple="multiple" placeholder={product.image ? product.image : null} type="file" name="image" id="image" onChange={handleChangeImage} />
         </label>
         <div className="update-product-submit">
           {/* <NavLink to="/products"> */}
